b821d16c5028033d29dfded035505efa355df9fd
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2021-10-23 10:17:07 +0200

    Set correct content-type header when delivering static files

--- src/web.rs
+++ src/web.rs
@@ -8,23 +8,39 @@ pub async fn route_request(req: Request) -> Result<Response> {
     let uri = req.uri();
     assert!(uri.path().starts_with('/'));
     let path = &uri.path()[1..];
-    let contents = if let Some(file) = WEB_DIR.get_file(path) {
-        Some(file.contents)
-    } else if let Some(file) = WEB_DIR.get_file("index.html") {
-        Some(file.contents)
-    } else {
-        None
-    };
 
-    if let Some(contents) = contents {
-        hyper::Response::builder()
-            .status(StatusCode::OK)
-            .body(Body::from(contents))
-            .map_err(|err| err.into())
-    } else {
-        hyper::Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::from("NOT FOUND"))
-            .map_err(|err| err.into())
-    }
+    let response = WEB_DIR.get_file(path)
+        .map(|file| {
+            // Explicit request for an existing file.
+            let content_type = match path.rsplit('.').next().unwrap() {
+                "html" => "text/html",
+                "js" => "text/javascript",
+                "css" => "text/css",
+                "txt" => "text/plain",
+                _ => "application/octet-stream",
+            };
+            hyper::Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", content_type)
+                .body(Body::from(file.contents))
+        })
+        .or_else(|| {
+            // Fallback path just returns index.html so we can handle most routing in the frontend.
+            WEB_DIR.get_file("index.html")
+                .map(|file| {
+                    hyper::Response::builder()
+                    .status(StatusCode::OK)
+                    .header("Content-Type", "text/html")
+                    .body(Body::from(file.contents))
+                })
+        })
+        .unwrap_or_else(|| {
+            // Fallback for when index.html does not exist, which may happen in some development
+            // setups.
+            hyper::Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from("NOT FOUND"))
+        })?;
+
+    Ok(response)
 }
