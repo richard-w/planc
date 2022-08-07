0aaafb768b5efdbb46ec8d61c58d54b401361282
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2022-08-07 13:26:54 +0200

    Setup frontend router

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,9 +1,36 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(route: &Route) -> Html {
+    match route {
+        Route::Home => {
+            html! {
+                <h1>{"Planc"}</h1>
+            }
+        }
+        Route::NotFound => {
+            html! {
+                <h1>{"Not Found"}</h1>
+            }
+        }
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{"Planc"}</h1>
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
     }
 }
 
