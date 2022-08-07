2be63246f7f54ed33e7ffd9a612b2fbaef62cdcf
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2022-08-07 09:11:56 +0200

    build.rs: Fix formatting

--- build.rs
+++ build.rs
@@ -4,13 +4,14 @@ fn main() {
     // Build the frontend
     let frontend_build_exit = Command::new("trunk")
         .current_dir("web")
-        .args([
-            "build",
-        ])
+        .args(["build"])
         .status()
         .expect("failed to run frontend build");
     if !frontend_build_exit.success() {
-        panic!("building the frontend failed: {:?}", frontend_build_exit.code());
+        panic!(
+            "building the frontend failed: {:?}",
+            frontend_build_exit.code()
+        );
     }
     // Rerun if frontend files change
     println!("cargo:rerun-if-changed=web/src");
